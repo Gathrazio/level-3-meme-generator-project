040fb87eb0558b493efe23fe0acc914b9628c784
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-06-11 00:40:43 -0600

    implemented the meme generator list functionality

--- level-3-meme-generator-project/src/components/MemeElement.jsx
+++ level-3-meme-generator-project/src/components/MemeElement.jsx
@@ -4,22 +4,50 @@ import trollface from '../assets/trollface-transparent.png' // temporary
 
 export default function MemeElement (props) {
 
-    // use the received props to paint a created meme to the screen
-    // also pass in the handleEdit function to the buttons using the onClick property
+    /*
+
+    const memeElements = props.savedMemes.map(meme => <MemeElement 
+        key={meme.id} 
+        data={meme} --> id, stateCounter, topText, bottomText
+        utilities={props.utilities}
+    />)
+
+    */
+
+    function handleSaveEditButton (e) {
+        const {value} = e.target;
+        if (value === "Edit") {
+            props.utilities.initiateEditState(props.data[`topText${props.data.stateCounter}`], props.data[`bottomText${props.data.stateCounter}`], props.data.stateCounter)
+        } else {
+            props.utilities.saveEdit(props.data.id, props.data.stateCounter)
+        }
+    }
+
+    let memeTextTop;
+    let memeTextBottom;
+
+    if (props.utilities.editSaveButtonValue != "Edit" && props.utilities.currentTexts[`topText${props.data.stateCounter}`]) {
+        memeTextTop = props.utilities.currentTexts[`topText${props.data.stateCounter}`];
+        memeTextBottom = props.utilities.currentTexts[`bottomText${props.data.stateCounter}`];
+    } else {
+        memeTextTop = props.data[`topText${props.data.stateCounter}`]
+        memeTextBottom = props.data[`bottomText${props.data.stateCounter}`]
+    }
+
 
     return (
         <div className="meme-element-wrapper">
             <div className="inputs-button-wrapper">
-                <Inputs />
+                {props.utilities.showEditField && props.utilities.currentTexts[`topText${props.data.stateCounter}`] && <Inputs displayTexts={props.utilities.currentTexts} stateCounter={props.data.stateCounter} handleChange={props.utilities.handleEditInputChange}/>}
                 <div className="button-wrapper">
-                    <button className='get-new-image button'>Edit</button>
-                    <button className='submit button'>Delete</button>
+                    <button className='get-new-image button' onClick={handleSaveEditButton} value={props.utilities.editSaveButtonValue}>{props.utilities.editSaveButtonValue}</button>
+                    <button className='submit button' onClick={() => props.utilities.deleteMeme(props.data.id)}>Delete</button>
                 </div>
             </div>
             <div className="meme-block-wrapper">
                 <img className="current-meme-image" src={trollface} />
-                <h2 className='meme-text meme-text-top'>Top Text</h2>
-                <h2 className='meme-text meme-text-bottom'>Bottom Text</h2>
+                <h2 className='meme-text meme-text-top'>{memeTextTop}</h2>
+                <h2 className='meme-text meme-text-bottom'>{memeTextBottom}</h2>
             </div>
         </div>
     )
